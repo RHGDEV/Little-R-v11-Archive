e8d6b97b4a874c480f9d3bde8cc4fe5b9657a004
RHG <huskey.rusty@gmail.com>
2017-12-01 22:44:17 -0500

    Update checkPremium.js

--- util/checkPremium.js
+++ util/checkPremium.js
@@ -1,12 +1,14 @@
-const premiumServers = require("../config.json").premiumServers
+const config = require("../config.json")
+const premiumServers = config.premiumServers
+const creatorid = config.creatorid
 
 let pre = `\n💰 **This server's Premium Status:** \nPREMIUM\n\nThanks for donating for this feature!`
 let def = `\n💰 **This server's Premium Status:** \nDEFAULT\n\nTo add a Premium status to your server go here! =>> [Buy Premium](https://littler.tk/premium)`
 let featureDef = `\n💰 **This feature is Premium only:**\n\nTo add a Premium status to your server go here! =>> [Buy Premium](https://littler.tk/premium)`
 
 module.exports = (bot, message, sendMessage, feature) => {
-  if (sendMessage == true) {
-    if (message.author.id == 140487710727995392) {
+  if (sendMessage !== true) {
+    if (message.author.id == creatorid) {
         return true
     }
   }
