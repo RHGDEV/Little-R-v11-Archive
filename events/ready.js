d1a9e66ef271d9b4cd9f0f1dd3de77d22627b022
RHG <huskey.rusty@gmail.com>
2017-12-13 19:59:36 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,8 +6,9 @@ module.exports = (bot) => {
   console.log(`Loaded commands!\n`);
   console.log(`~ Prefix: ${config.default.prefix}`);
   console.log(`~ ${bot.guilds.array().length} Guilds ${bot.channels.array().length} Channels ${bot.users.array().length} Users\n`)
-  bot.user.setGame(`Loading ${bot.user.username}... | littler.tk`)
-
+ // bot.user.setGame(`Loading ${bot.user.username}... | littler.tk`)
+  bot.user.setGame(`l;help | littler.tk`)
+  
   // Whitelist
   if (whiteList.whitelist == true) {
     bot.guilds.forEach(async (guild, id) => {
@@ -28,6 +29,8 @@ module.exports = (bot) => {
   console.log(`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`)
   console.log(`${bot.user.username} loaded!\n`)
 
+  //FIX SOON
+  return
   //Status Rotator
   gameval = 0
   setInterval(() => {
