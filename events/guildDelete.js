66591ca10f064ed0e0ca929d5c96dc745d371de8
RHG <huskey.rusty@gmail.com>
2018-01-07 12:28:58 -0500

    Delete guildDelete.js

--- events/guildDelete.js
+++
@@ -1,7 +0,0 @@
-const { eventlogger } = require("../util/eventLogger.js")
-
-module.exports = (bot, guild) => {
-  console.log(`[LEFT] [#${guild.memberCount}] ${guild.name}, ${guild.id}`)
-  eventlogger(bot, `**Guild Leave**\n\n**Guild:** ${guild.name}\n**Owner:** ${guild.owner.user.tag}\n**Large:** ${guild.large}\n**Member Count:** ${guild.memberCount}\n\n**Total Guilds:** ${bot.guilds.array().length}`, guild.iconURL)
-
-};
